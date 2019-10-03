ca64e143528a13a49afd5a10fd1874e472609538
bejaoui <f.bejaoui1@gmail.com>
2019-10-03 16:32:51 +0200

    changement port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require ("express");
 const cors = require ("cors");
 const bodyParser = require ("body-parser");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 app.use(cors({origin: 'http://localhost:8080'}));
